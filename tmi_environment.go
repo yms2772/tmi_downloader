721e90bb6067376fbd2676ba9fbb4722b48ddef5
Mokky <a53312773@gmail.com>
2020-06-11 12:31:08 +0900

    20200611 업데이트
    버전명 수정

--- tmi_environment.go
+++ tmi_environment.go
@@ -23,7 +23,7 @@ const ( // API
 )
 
 var ( // Main 변수
-	version        = "20200529"
+	version        = "20200611"
 	title          = "TMI Downloader"
 	dirTemp        = VarOS("dirTemp")
 	dirBin         = VarOS("dirBin")
